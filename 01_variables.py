a37bc64bd84e17375fa1bd692ec125894087788c
Criss-pyt <cristopherpro2021@gmail.com>
2024-10-31 18:56:43 -0700

    Update 01_variables.py

--- 01_variables.py
+++ 01_variables.py
@@ -1,8 +1,5 @@
 # Variables 
 
-from operator import le 
-
-
 my_string_variable= "My string variable"
 print(my_string_variable)
 
@@ -18,6 +15,12 @@ print(my_bool_variable)
 
 # Concatenacion de variables en un print
 print(my_string_variable, str(my_int_variable), my_bool_variable)
+print("Este es el valor de:",my_bool_variable)
 
 # Algunas funciones de el sistema 
 print(len(my_string_variable))
+# len = contar 
+
+# Variables en una sola linea 
+name, surname, alias, age = "Brais", "Moure", "MoureDev", 35
+print("Me llamo:", name, surname, ". Mi edad es:" , age, ". Y mi alias es:", alias )
