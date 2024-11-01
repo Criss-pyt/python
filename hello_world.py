3faf7024d6400b893fb2a749cf68ad848d39d037
Criss-pyt <cristopherpro2021@gmail.com>
2024-10-31 17:08:40 -0700

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,16 @@
 # Este es un comentario: Hola mundo
 #Nuestro Hola mundo en Python
 print ("Hola Python")
+print ('Hola Python')
 
 """
 Este es un
 comentario 
 en varias lineas 
 """
+
+'''
+Este tambien es un
+comentario 
+en varias lineas 
+'''
