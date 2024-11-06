3c8d0e8ccb3c4f3e634bcf8779755e5380a650d7
Criss-pyt <cristopherpro2021@gmail.com>
2024-11-06 23:22:26 +0000

    agregando el problema

--- example.py
+++ example.py
@@ -1,1 +1,12 @@
-print("Hello")
+#Cuenta de restaurante
+
+monto= input ("¿Cual es el monto de su cuenta?")
+porcentaje= input ("¿Cuanto porcentaje de propina quisiera dar ?")
+print(monto)
+print(porcentaje)
+
+cuenta_total=(monto * (porcentaje / 100))
+print(cuenta_total)
+
+print(f"El total a pagar de su comida es de (cuenta_total)")
+
