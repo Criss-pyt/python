37afc2334f0845a876bb3d6233df6206beeaa627
Criss-pyt <cristopherpro2021@gmail.com>
2024-11-06 23:34:27 +0000

    terminamos el problema

--- example.py
+++ example.py
@@ -3,6 +3,8 @@
 monto= float(input ("¿Cual es el monto de su cuenta?"))
 porcentaje= float(input ("¿Cuanto porcentaje de propina quisiera dar ?"))
 
+
 total= monto * (porcentaje / 100)
 
 print(f"El total a pagar de su comida es de :", (total + monto))
+
