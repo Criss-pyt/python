d253a7a3ff8d7e7bc071b4f5537c5f9372cacc98
Criss-pyt <cristopherpro2021@gmail.com>
2024-11-06 18:17:17 -0600

    Create adivina_el_numero.py

---
+++ adivina_el_numero.py
@@ -0,0 +1,6 @@
+numero1= float(input("Ingresa un numero :"))
+numero2= float(input("Ingresa otro numero:"))
+
+if numero1 > numero2:print("El primer numero es mayor")
+elif numero1 < numero2:print("El segundo numero es mayor")
+else:print("Ambos numeros son iguales")
