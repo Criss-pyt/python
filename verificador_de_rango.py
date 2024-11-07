b40a7567d39b98f32f3f0fa81b0097fdc9ea24b9
Criss-pyt <cristopherpro2021@gmail.com>
2024-11-06 18:05:54 -0600

    Create verificador_de_rango.py

---
+++ verificador_de_rango.py
@@ -0,0 +1,8 @@
+#comando float: ayuda a poder convertir los numeros a decimales 
+#comando imput: ''''ayuda a poder realizar un problema matematico 
+#convirtiendo el numero de la cadena de texto ingresada por el input
+
+numero = float(input("Pon un numero aleatorio:"))
+
+if 10 <= numero <= 50: print("El numero esta en el rango")
+else: print("El numero no esta en el rango")
