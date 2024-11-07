4c0f3e85fda3c767626210ab0e642883266149a6
Criss-pyt <cristopherpro2021@gmail.com>
2024-11-07 00:35:35 +0000

    listos

---
+++ par_o_inpar.py
@@ -0,0 +1,7 @@
+'''el "int"tiene el mismo significado que el float, la diferencia es
+ que se puede usar para numeros enteros
+ a diferencia de el float que es para numeros decimales'''
+ 
+numero = int(input("Coloca un numero : "))
+if numero % 2 == 0: print("El numero es par")
+else: print("El numero es impar")
