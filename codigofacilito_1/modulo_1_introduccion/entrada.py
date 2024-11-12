e58af18dd49bb90142143bb37a1d3d6299c333fe
Criss-pyt <cristopherpro2021@gmail.com>
2024-11-12 20:05:20 +0000

    Terminado el dia de hoy

--- codigofacilito_1/modulo_1_introduccion/entrada.py
+++ codigofacilito_1/modulo_1_introduccion/entrada.py
@@ -1,14 +1,46 @@
 #Valores de entrada que provienen de diferentes lugares 
 #El dia de hoy arenderemos a ingresar por via teclado
-
+'''
 nombre_completo = input("Ingresa tu nombre completo : ") # Siempre regresara tipo de dato 'str'
 #Input recibe como argumento de forma opcional un mensaje
 #siempre colocar un mensaje para poder ingresar el valor
 
 print(nombre_completo)
 print(type(nombre_completo))
- 
 
- edad_player = input("ingresa tu edad: ")
- print(edad_player)
- print(type(edad_player))
+
+edad_player =input("ingresa tu edad: ")
+edad_player = int(edad_player)
+
+print(edad_player)
+print(type(edad_player))
+
+altura = input("Ingresa tu altura : ")
+#foat permite crear un nuevo flotante a partir de un string
+altura = float(altura)
+
+print(altura)
+print(type(altura))
+
+autorizacion_programa = input("Autorizas el programa? (si/no)")
+autorizacion_programa = autorizacion_programa == 'si'
+#Si la respuesta fuese 'no' resultado sera False
+print(autorizacion_programa)
+'''
+
+''' son muchas lineas 
+de codigo, por lo cual se puede hacer
+mas simplificado haciendo lo siguiente :'''
+
+nombre_completo = input("Ingresa tu nombre completo : ") # Siempre regresara tipo de dato 'str'
+
+edad_player =int(input("ingresa tu edad: "))
+
+altura_player = float(input("Ingresa tu altura : "))
+
+autorizacion_programa = input("Autorizas el programa? (si/no) : ") == 'si'
+
+print(nombre_completo)
+print(edad_player)
+print(altura_player)
+print(autorizacion_programa)
