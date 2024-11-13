0c8f1fdc1e158ded21629f8009014f6a650cfecf
Criss-pyt <cristopherpro2021@gmail.com>
2024-11-13 19:30:35 +0000

    cambios

---
+++ codigofacilito_1/modulo_1_introduccion/calculadora.py
@@ -0,0 +1,31 @@
+print("Esta sera una calculadora, favor ingresar los datos a calcular : ")
+
+primer_valor = float(input("Ingresa el primer valor : "))
+
+segundo_valor = float(input("Ingresa el segundo valor : "))
+
+print(primer_valor)
+print(segundo_valor)
+
+'''
+operacion_total =(primer_valor) (valor_de_operacion) (segundo_valor)
+print(operacion_total)
+'''
+
+'''
+print( "resultado :" + (primer_valor))
+'''
+'''
+print( f"resultado : {primer_valor}  {valor_de_operacion}  {segundo_valor}") 
+'''
+print()
+
+print("Resultado suma")
+print(primer_valor + segundo_valor)
+print("Resultado resta")
+print(primer_valor - segundo_valor)
+print("Resultado ultiplicacion")
+print(primer_valor * segundo_valor)
+print("Resultado division")
+print(primer_valor / segundo_valor)
+
---
+++ codigofacilito_1/modulo_1_introduccion/mutiples.py
@@ -0,0 +1,13 @@
+'''
+nombre = 'Criss'
+apellido = 'Flores'
+titulo = 'Mr.'
+'''
+#Esto se puede comprimir de la siguiente forma
+
+nombre, apellido, titulo = "Criss", "Flores", "Mr."
+#No crear muchas variables 4 a lo maximo y que siempre tengan correlacion de el mismo tema
+print(nombre)
+print(apellido)
+print(titulo)
+
