63626af404f3d7da59af9475cfc82cbcb48ecce0
Criss-pyt <cristopherpro2021@gmail.com>
2024-11-14 19:23:59 +0000

    no los habia subido

---
+++ codigofacilito_1/modulo_1_introduccion/multiples2.py
@@ -0,0 +1,9 @@
+calificacion = 45
+if calificacion == 10:
+    print("Feliciadades, aprobastes la materia con una calificacion perfecta")
+elif calificacion == 9 or calificacion == 8:
+    print("Felicidades, aprobastes la materia.")
+elif calificacion == 7 or calificacion == 6:
+    print("Aprobastes la materia.")
+else:
+    print("Reprobastes la materia :c ")
